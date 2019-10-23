1566a35a6320c2f793f4aec4d390693d7ec36901
bikundo <allanbmageto@gmail.com>
2019-10-23 17:34:07 +0300

    Comments, medical and appointments enpoint created

--- App/Router/router.js
+++ App/Router/router.js
@@ -1,9 +1,22 @@
 UserController = require('../Controllers/user_controller');
+AppointmentController = require('../Controllers/appointment_controller');
+MedicalHistoryController = require('../Controllers/medical_history');
 
 module.exports = (app) => {
   app.route('/').get((req, res) => {
     res.send('Welcome to TCC');
   });
+
+  //Users Endpoint
   app.route('/signin').post(UserController.signin);
   app.route('/signup').post(UserController.signup);
+
+
+  //Appointments endpoint
+  app.route('/getAllAppointments').get(AppointmentController.getAllAppointments);
+  app.route('/createAppointment').post(AppointmentController.createAppointment);
+
+  //Medical History Endpoints
+  app.route('/getAllMedicalHistory').get(MedicalHistoryController.getAllMediacalHistories);
+  app.route('/createMedicalHistory').post(MedicalHistoryController.createMedicalHistory);
 };
